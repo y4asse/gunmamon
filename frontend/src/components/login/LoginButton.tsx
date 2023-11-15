14035883bb62499e9e7519295fbdb6e3f0cb88a7
KanaFujitani <cattleya.leya0@gmail.com>
2023-11-15 20:31:23 +0900

    fix: ボタンの形と色の修正

--- frontend/src/components/login/LoginButton.tsx
+++ frontend/src/components/login/LoginButton.tsx
@@ -11,9 +11,14 @@ const LoginButton = () => {
 
   return (
     <>
-      <button className="shadow px-3 py-2 rounded-lg flex items-center text-xl mx-auto m-6 gap-3">
-        <a href={googleAuthUrl}>Google認証</a>
-      </button>
+      <a
+        href={googleAuthUrl}
+        className="inline-flex items-center px-5 py-3 text-base font-medium text-center text-white bg-violet-700 rounded-lg
+        hover:bg-violet-800 focus:ring-4 focus:outline-none focus:ring-violet-300 dark:bg-violet-600 dark:hover:bg-violet-700
+        dark:focus:ring-blue-800"
+      >
+        Google認証
+      </a>
     </>
   )
 }
