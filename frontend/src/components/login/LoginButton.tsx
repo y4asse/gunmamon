6015eb855b23e5641bfb308771e1b655225e0fff
y4asse <yasse0218@icloud.com>
2023-11-16 11:10:30 +0900

    chore:スキーマ変更、scope変更

--- frontend/src/components/login/LoginButton.tsx
+++ frontend/src/components/login/LoginButton.tsx
@@ -3,11 +3,16 @@ import { useRouter } from 'next/router'
 const LoginButton = () => {
   const redirect_uri = process.env.REDIRECT_URI
   const client_id = '171244859464-tmrgdbtp4c26j6u8s5pd71o36utcdjbq.apps.googleusercontent.com'
-  const scope = 'https://www.googleapis.com/auth/fitness.activity.read'
+  const scope = [
+    'https://www.googleapis.com/auth/userinfo.email',
+    'https://www.googleapis.com/auth/fitness.activity.read'
+  ]
   const response_type = 'code'
   const access_type = 'offline'
   const include_granted_scope = 'true'
-  const googleAuthUrl = `https://accounts.google.com/o/oauth2/v2/auth?scope=${scope}&access_type=${access_type}&include_granted_scope=${include_granted_scope}&response_type=${response_type}&redirect_uri=${redirect_uri}&client_id=${client_id}&prompt=consent`
+  const googleAuthUrl = `https://accounts.google.com/o/oauth2/v2/auth?scope=${scope.join(
+    '+'
+  )}&access_type=${access_type}&include_granted_scope=${include_granted_scope}&response_type=${response_type}&redirect_uri=${redirect_uri}&client_id=${client_id}`
 
   return (
     <>
