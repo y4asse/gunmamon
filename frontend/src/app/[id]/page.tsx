59fbbd3cb22b2218936b16dfcf01f257621fc49b
Your Name <flaghatarealcode@gmail.com>
2023-11-18 08:44:58 +0900

    コンポーネントを画像と文章の混載に対応

--- frontend/src/app/[id]/page.tsx
+++ frontend/src/app/[id]/page.tsx
@@ -1,20 +1,49 @@
 import Person from '@/components/animation/Camping';
 import React from 'react';
 
-const Step = ({ color, title, content }) => {
-  const contentLines = content.split('<br>');
+const Step = ({ color, title, contents, width, height }) => {
+  const isImage = (url) => {
+    const extension = url.split('.').pop().toLowerCase();
+    const imageExtensions = ['jpg', 'jpeg', 'png', 'gif', 'bmp'];
+    return imageExtensions.includes(extension);
+  };
+  const imageStyle = {
+    width: width ? `${width}px` : 'auto',
+    height: height ? `${height}px` : 'auto',
+    maxWidth: '100%',
+    maxHeight: '100%',
+  };
+
+  const DOMs = contents.map((item, index) => (
+    <React.Fragment key={index}>
+      {!isImage(item) ? (
+        <div className="m-2 font-normal">
+          {item.split('<br>').map((sentence, index) => (
+            <React.Fragment key={index}>
+              {sentence}
+              <br />
+            </React.Fragment>
+          ))}
+        </div>
+      ) : (
+        <img
+          src={item}
+          alt={`Custom Image ${index}`}
+          className="m-2 mx-auto"
+          style={imageStyle}
+        />
+      )}
+      <br />
+    </React.Fragment>
+  ));
+
   return (
     <div className={`rounded-lg border p-2 bg-${color} mt-5 px-10`}>
       <div className="mb-4 font-bold text-4xl">
         {title}
       </div>
       <div className="m-2 font-normal">
-      {contentLines.map((line, index) => (
-          <React.Fragment key={index}>
-            {line}
-            <br />
-          </React.Fragment>
-        ))}
+        {DOMs}
       </div>
     </div>
   );
@@ -45,11 +74,19 @@ const Page = async ({ params }: { params: { id: string } }) => {
           <button className="rounded bg-indigo-500 py-1 px-4">コピーする</button>
         </div>
         <div>
-          <Step color="blue-200" title="手順1" content="あああああああ<br>あああああ<br>あああああ<br>あああああ" />
+          <Step color="blue-200" title="手順1" contents={
+            ["あああああああ<br>あああああ<br>あああああ<br>あああああ"
+          ,"https://www.pokemon.co.jp/ex/sv/assets/img/pokemon/220227_02/visual.png"
+          ,"あああああああ<br>あああああ<br>あああああ<br>あああああ"
+          ]} width="50" height="50" />
           <div className="m-2 font-bold">
             ↓
           </div>
-          <Step color="blue-300" title="手順2" content="あああああああ<br>あああああ<br>あああああ<br>あああああ" />
+          <Step color="blue-200" title="手順1" contents={
+          ["あああああああ<br>あああああ<br>あああああ<br>あああああ"
+          ,"https://www.pokemon.co.jp/ex/sv/assets/img/pokemon/220227_02/visual.png"
+          ]
+          } width="50" height="50" />
           <div className="mb-5"></div>
         </div> 
       </div>
