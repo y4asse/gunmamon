f1e0783716026908fd6b28dfa4a3551039e6e4a6
y4asse <yasse0218@icloud.com>
2023-11-18 01:36:49 +0900

    feat: 草の色カスタマイズプレビュー画面なのだ

--- frontend/src/app/[id]/createURL/page.tsx
+++ frontend/src/app/[id]/createURL/page.tsx
@@ -1,18 +1,39 @@
-import CopyButton from '@/components/mypage/CopyButton'
-import React from 'react'
+'use client'
 
-const Page = async ({ params }: { params: { id: string; url: string } }) => {
+import CopyButton from '@/components/mypage/CopyButton'
+import CustomColor from '@/components/mypage/CustomColor'
+import React, { useState } from 'react'
+export const colorTypes = ['red', 'pink', 'orange', 'yellow', 'green', 'light-blue', 'blue', 'purple'] as const
+export type Colors = (typeof colorTypes)[number]
+
+const Page = ({ params }: { params: { id: string; url: string } }) => {
   const { id } = params
-  const url = `https://high-wave-403814.an.r.appspot.com?id=${id}`
+  const [colorType, setColorType] = useState<Colors>('orange')
+  const [bgColor, setBgColor] = useState('#ffffff')
+  const [textColor, setTextColor] = useState('#000000')
+  const url = `${process.env.NEXT_PUBLIC_API_URL}?id=${id}&color_type=${colorType}&bg_color=${bgColor}&text_color=${textColor}`
+  const validUrl = url.replaceAll('#', '%23')
+  const copyText = `![https://gunmamon.vercel.app](${validUrl})`
 
   return (
     <>
       {/* GitHub用のURL */}
-      <div className="mt-10 text-center">
-        <h1 className="text-2xl font-bold text-center mb-7">あなたのURL</h1>
-        <div className="shadow-xl p-5 rounded-xl whitespace-nowrap bg-gray-500 mt-2 mb-7 overflow-x-auto">{`![https://gunmamon.vercel.app](https://high-wave-403814.an.r.appspot.com?id=${id})`}</div>
+      <CustomColor
+        colorType={colorType}
+        bgColor={bgColor}
+        setColorType={setColorType}
+        setBgColor={setBgColor}
+        textColor={textColor}
+        setTextColor={setTextColor}
+      />
+      <div className="mt-10 text-center bg-white p-5 rounded-xl shadow-xl border-2 border-gray-400 mb-20">
+        <h1 className="text-2xl font-bold text-center text-black">GitHub用のURL</h1>
+        <p className="text-black mt-3">(このテキストをGitHubに貼り付けるとREADMEに運動の草を生やすことができます)</p>
+        <div className="shadow-xl p-5 rounded-xl whitespace-nowrap bg-gray-500 mt-2 mb-7 overflow-x-auto">
+          {copyText}
+        </div>
         <div>
-          <CopyButton copyText={url} />
+          <CopyButton copyText={copyText} />
         </div>
         {/* <div className="mt-10">TODO ここにGitHubに表示する説明を書く、、、</div> */}
       </div>
