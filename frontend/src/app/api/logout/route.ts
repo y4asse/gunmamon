76a66e554d1dbf021ca9129d60b052e9ec33dc5a
y4asse <yasse0218@icloud.com>
2023-11-18 02:07:41 +0900

    refactor: 関数の名前これじゃないとだめなのだ

--- frontend/src/app/api/logout/route.ts
+++ frontend/src/app/api/logout/route.ts
@@ -1,12 +1,8 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { cookies } from 'next/headers';
+import { NextApiRequest, NextApiResponse } from 'next'
+import { cookies } from 'next/headers'
 
-export default function handleLogout(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
-    // クッキーを削除する処理
-    cookies().delete('session_token');
-    res.status(200).json({ message: 'Logged out' });
-  } else {
-    res.status(405).end('Method Not Allowed');
-  }
+export function POST(req: NextApiRequest, res: NextApiResponse) {
+  // クッキーを削除する処理
+  cookies().delete('session_token')
+  return Response.json({ message: 'Logged out' })
 }
