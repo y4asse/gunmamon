409f61340e5a6d31a5446cbf7f459aa462332ee0
KanaFujitani <cattleya.leya0@gmail.com>
2023-11-18 13:40:06 +0900

    fix: ログインページの微調整

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="w-full max-w-lg bg-white border border-gray-200 rounded-xl shadow dark:bg-gray-800 dark:border-gray-700">
         <div className="flex justify-end px-4 pt-4"></div>
         <div className="flex flex-col items-center pb-10">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#6435D1" className="w-20 h-20 mt-12 mb-4">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#6366f1" className="w-20 h-20 mt-12 mb-4">
             <path
               fillRule="evenodd"
               d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z"
@@ -23,7 +23,7 @@ export default function Home() {
           <span className="mt-6 mb-7">
             <p className="mb-1 text-sm text-gray dark:text-white mt-3">
               プライバシーポリシーについては
-              <Link className="underline" href={'/about/privacy'}>
+              <Link className="underline" href={'/about'}>
                 こちら
               </Link>
               をご確認ください。
